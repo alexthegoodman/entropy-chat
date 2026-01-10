416f7fac9d8c1f62762b41ae6aa44b00ead9f547
Alex Woodman <alexthegoodman@gmail.com>
2026-01-09 19:37:00 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use leptos::prelude::*;
+use leptos::wasm_bindgen::JsCast;
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, Entropy Chat!"</p> })
+    // Find the container element by its ID
+    let container = web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .get_element_by_id("leptos-container") // Use the ID from index.html
+        .expect("Did not find the element with ID 'leptos-container'")
+        .unchecked_into::<web_sys::HtmlElement>();
+
+    let _ = leptos::mount::mount_to(container, || view! { <p>"Hello, Entropy Chat!"</p> });
 }
